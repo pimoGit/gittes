004884d9018e3cab5cdb4741b40dadce8b5136bc
Simone Icardi <info@simoneicardi.com>
2019-07-03 14:51:46 +0200

    new live coding update

--- main.js
+++ main.js
@@ -1,39 +1,166 @@
+
+
+var listaspesa = [
+    "latte",
+    "pane",
+    "fonzies",
+    "carne",
+    "seppia",
+    "uova"
+]
+
 var elemento = document.getElementById("mio_id");
-var risultato, sceltapd, sceltaNumd, scelatNumcpu, somma ;
+
+var contenutoTemp;
+
+var newelement = prompt("inserisci qualcosa da comprare");
+
+listaspesa.push(newelement);
 
 
-// utente sceglie pari o dispari
-sceltapd = prompt("scegli pari o dispari");
-console.log(sceltapd);
+ for (var i = 0; i < listaspesa.length; i++) {
+    //  console.log(listaspesa[i]);
 
-// utente sceglie numero da 1 a 5
-sceltaNumd = parseInt(prompt("scegli unnumero da 1 a 5"));
-console.log(sceltaNumd);
+    contenutoTemp = elemento.innerHTML;
+    console.log(contenutoTemp);
 
-// cpu genera numero da 1 a 5
-scelatNumcpu = Math.floor(Math.random() * 5) + 1;
-console.log(scelatNumcpu);
-
-// sommo i 2 numeri e salvo il risultato in "somma"
-somma = sceltaNumd + scelatNumcpu;
-console.log(somma);
-
-// verifico che la somma sia pari o dispari, salvandomi il valore "risultato"
- if (somma % 2 === 0) {
-      risultato = "pari";
- } else {
-      risultato = "dispari";
-     //dispari
+    elemento.innerHTML = contenutoTemp + "<li>" + listaspesa[i] + "</li>";
  }
 
-// var temvar = somma % 2;
-// console.log(temvar);
+//  elemento.innerHTML =  listaspesa;
 
 
-// stampo vincitore confrontando  che la scelta iniziale (utente) corrisponda o meno al valore "risultato"
+// var contenuto = [];
 
-if (risultato === sceltapd) {
-    elemento.innerHTML = "hai vinto";
-} else {
-    elemento.innerHTML = "hai perso!!!!";
-}
+// for (var i = 0; i < listaspesa.length; i++) {
+//     //  console.log(listaspesa[i]);
+
+
+
+//     elemento.innerHTML = "<li>" + contenuto[j] + "</li>";
+//  }
+
+//  for(var j = 0; j < listaspesa.length; j++){
+//     contenuto[j] = listaspesa[i];
+//   }
+
+
+
+// var nomi = ["nome1","nome2","nome3","nome4","nome5"];
+
+// //  for (var i = 0; i < nomi.length; i++){ 
+// //      console.log(nomi[i]);
+// //  }
+
+
+// //  var i = 0;
+//  while (trovato === true){ 
+//     console.log(nomi[i]);
+//     if(qualcosa){
+//         trovato = false;
+//     }
+// }
+
+
+// var numarray = [3,6,2,17,22,5];
+
+// for (var i = 0; i < numarray.length; i++){
+
+//     if (numarray[i] % 2 != 0){
+//         console.log(numarray[i]);
+//     }
+    
+// }
+
+
+
+
+// for (var i = 1000; i >= 0; i--){
+
+//     console.log(i);
+// }
+
+// var mioarray = ["nome1","nome2","nome3","nome4","nome5"];
+
+// console.log(mioarray.length);
+
+// mioarray.push("nomaaltro");
+
+// console.log(mioarray.length);
+
+// for (var i = 0; i < mioarray.length; i++) {
+//     console.log(mioarray[i]);
+// }
+
+
+
+
+// var nomi = [];
+
+// nomi[0] = "Mario";
+// nomi[1] = "Altro";
+// nomi[2] = "Altro ancora";
+
+
+// var numero = parseInt(prompt("numero tra 0 e 2!"));
+// console.log("nuemro inserito", numero);
+
+// console.log(nomi[numero]);
+
+// console.log(nomi);
+
+// nomi.push("alvolo!");
+
+// document.getElementById("mio_id").innerHTML = nomi;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var elemento = document.getElementById("mio_id");
+// var risultato, sceltapd, sceltaNumd, scelatNumcpu, somma ;
+
+
+// // utente sceglie pari o dispari
+// sceltapd = prompt("scegli pari o dispari");
+// console.log(sceltapd);
+
+// // utente sceglie numero da 1 a 5
+// sceltaNumd = parseInt(prompt("scegli unnumero da 1 a 5"));
+// console.log(sceltaNumd);
+
+// // cpu genera numero da 1 a 5
+// scelatNumcpu = Math.floor(Math.random() * 5) + 1;
+// console.log(scelatNumcpu);
+
+// // sommo i 2 numeri e salvo il risultato in "somma"
+// somma = sceltaNumd + scelatNumcpu;
+// console.log(somma);
+
+// // verifico che la somma sia pari o dispari, salvandomi il valore "risultato"
+//  if (somma % 2 === 0) {
+//       risultato = "pari";
+//  } else {
+//       risultato = "dispari";
+//      //dispari
+//  }
+
+// // var temvar = somma % 2;
+// // console.log(temvar);
+
+
+// // stampo vincitore confrontando  che la scelta iniziale (utente) corrisponda o meno al valore "risultato"
+
+// if (risultato === sceltapd) {
+//     elemento.innerHTML = "hai vinto";
+// } else {
+//     elemento.innerHTML = "hai perso!!!!";
+// }
