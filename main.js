73b7b1e0eaef892792ba1df760f9bb61c617e8e7
Simone Icardi <info@simoneicardi.com>
2019-07-22 10:57:34 +0200

    new live coding templating js+html

--- main.js
+++ main.js
@@ -2,64 +2,34 @@
 
 $( document ).ready(function() {
 
-//funzione del next
 
-   $(".next").click(nextimg);
+   $("#sendmsg").click(
+      function(){
+         //salvo valore inserito dall'utente
+         var inputval = $("#msginput").val();
+         // console.log(inputval);
 
-
-
-   // function nextimg() {
-   //    var activeimg = $(".images img.active");
-   //    var activebully = $(".pallini span.active");
-
-   //    activeimg.removeClass("active");
-   //    activebully.removeClass("active");
-
-   //    if(activeitem.hasClass("last")) {
-   //       $(".pallini span.first").addClass("active");
-   //    } else {
-   //       activeimg.next("img").addClass("active");
-   //       activebully.next("span").addClass("active");
-
-   //    }
-
-
-   // }
+         //butto in pagina il messaggio
+         //   $(".container").append(
+         //      "<div class='msg'>" + inputval + "</div>"
+         //   );
+         
+         var msgelement = $("#template .msg").clone();
+         // console.log(msgelement);
+         msgelement.text(inputval);
+         $(".container").append(msgelement);
+         
+           
+           //pulisco l'input alla fine
+           $("#msginput").val("");
 
 
-   function nextimg() {
-      var activeitem = $(".images img.active, .pallini span.active");      
-      activeitem.removeClass("active");
-
-      if(activeitem.hasClass("last")) {
-         $("img.first, .pallini span.first").addClass("active");
-      } else {
-         activeitem.next().addClass("active");
       }
+   );
 
 
-   }
-
-
-
-   //funzione del prev
-
-   $(".prev").click(previmg);
-
-   function previmg(){
-      var activeimg = $(".images img.active");
-      activeimg.removeClass("active");
-
-      if(activeimg.hasClass("first")){
-         $("img.last").addClass("active");
-      } else {
-         activeimg.prev("img").addClass("active");
-      }
-
-   }
-
-
-
+   // var altrocoso = $("#template .altrodaclonare").clone();
+   
 
 
 });
