df9b764d376c71b8045c2f5bab683b4a5b26b355
Simone Icardi <info@simoneicardi.com>
2019-07-26 10:40:17 +0200

    new live coding templating+handlebars

--- main.js
+++ main.js
@@ -9,19 +9,41 @@ $( document ).ready(function() {
          var inputval = $("#msginput").val();
          // console.log(inputval);
 
-         //butto in pagina il messaggio
-         //   $(".container").append(
-         //      "<div class='msg'>" + inputval + "</div>"
-         //   );
          
-         var msgelement = $("#template .msg").clone();
-         // console.log(msgelement);
-         msgelement.text(inputval);
-         $(".container").append(msgelement);
+         // var msgelement = $("#template .msg").clone();
+         // // console.log(msgelement);
+         // msgelement.text(inputval);
+         // $(".container").append(msgelement);
+
+         var source   = $("#msgtemplate").html();
+         console.log(source);
+         
+         var template = Handlebars.compile(source);
+         console.log("iltemplate è" + template);
+
+         var context = {text: inputval, addclass: "sent", urlimg: "https://freewebsitetemplates.com/images/forum/500/ecologicalwebsitetemplate.jpg"};
+         var html    = template(context);
+
+         $(".container").append(html);
          
            
            //pulisco l'input alla fine
            $("#msginput").val("");
+
+
+           //messaggio di risaposta
+
+         setTimeout(reply,1000);
+
+         function reply(){
+
+         var contextrips = {text: "risposta pc! 'zzo vuoi!", addclass: "risp"};
+         var htmlrips    = template(contextrips);
+
+         $(".container").append(htmlrips);
+
+         }
+           
 
 
       }
