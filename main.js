1a3f4cb5942c193d2cc6b3bfec03650a79c13d44
Simone Icardi <info@simoneicardi.com>
2019-10-23 13:05:01 +0200

    while loop and some trick to undestrand better

--- main.js
+++ main.js
@@ -1,217 +1,110 @@
-// Crea un array di numeri, e crea un array nuovo contenente i numeri raddoppiati dell'originale 
-
-var nums = [1,4,7,9,5,2,8,9,10];
-
-var numsDouble = [];
-var numx2;
+// Stampare a schermo una lista della spesa
 
 
-for (var i = 0; i < nums.length; i++){
+var lista = ["Pane","latte","carne","proscuitto","guanciale","insalata"];
 
-    // numsDouble[i] = nums[i] * 2;
+var messagio = document.getElementById('messaggio');
 
-    numx2 = nums[i] * 2;
-
-    numsDouble.push(numx2);
-
-    console.log(numsDouble);
-    
-
-}
+var cosaPresente = false;
 
 
-// console.log("l'array di partenza è: ",nums, "l'array nuovo è: ", numsDouble);
+//chiedo a utente una cosa  da comprare
+var cosa = prompt("inserisci cosa da comprare");
 
 
 
 
-/*
-// Crea un array di numeri, e crea un array nuovo contenente solo i numeri pari dell'array originale
+// verifico che quella cosa non sia già presente nella lista
+// for (var j = 0; j < lista.length; j++){
 
-var nums = [1,4,7,9,5,2,8,9,10];
+//     if(cosa === lista[j]){
+//         cosaPresente = true;
+//     } 
 
-var numpari = [];
-
-for (var i = 0; i < nums.length; i++){
-
-    var itemLoop = nums[i];
-
-    if(itemLoop % 2 == 0){
-        numpari.push(itemLoop);
-    }
-
-}
-
-console.log("l'array di partenza è: ",nums, "l'array nuovo è: ", numpari);
-
-
-*/
-
-
-
-/*
-// Crea un array di numeri, e stampa solo gli elementi in posizione dispari
-
-// var nums = [1,4,7,9,5,2,8,9,10];
-var nums = ["1", "5", "4", "3","8"];
-
-console.log("array: ", nums);
-
-
-// for (var i = 1; i < nums.length; i = i+2){
-//     console.log(nums[i]);
-    
 // }
 
-for (var i = 0; i < nums.length; i++){
+var j = 0;
+while(j < lista.length && !cosaPresente ){
 
-    if(i % 2 !==0){
-        console.log(nums[i]);
-    }
+    if(cosa === lista[j]){
+        cosaPresente = true;
+    } 
+    console.log("la mia interazione è arrivata a: ",j);
     
+
+    j++;
 }
 
-*/
 
+
+
+
+
+// se è presente glielo dico
+if(cosaPresente === true){
+    messagio.innerHTML = "la cosa è già in lista";
+// sennò aggiungo alla lista
+} else {
+    messagio.innerHTML = "la cosa NON è già in lista";
+    lista.push(cosa);
+}
+
+
+
+
+
+
+
+// stampo elenco della lista
+var i = 0;
+var contenutoPrecedente;
+
+while (i < lista.length){
+    // console.log(lista[i]);
+    // document.getElementById('mioid').innerHTML += "<li>" + lista[i] + "</li>";
+
+    contenutoPrecedente = document.getElementById('mioid').innerHTML;
+
+    console.log("il valore all'interazione numero" + i + " di contenutoPrecedente è uguale a: ",contenutoPrecedente);
+    
+
+    document.getElementById('mioid').innerHTML = contenutoPrecedente + "<li>" + lista[i] + "</li>";
+
+    i++;
+}
 
 
 
 
 /*
-// Crea un array di numeri, e stampa solo i numeri dispari
+// traduci in un ciclo while
 
 
-var nums = [1,4,7,9,5,2,8,9,10];
+var nomi = ["nome1","nome2","nome3","nome4","nome5","nome6"];
 
-for (var i = 0; i < nums.length; i++){
-    
-    var arrayitemcilo = nums[i];
+// for (var i = 0; i < nomi.length; i++){ 
+//     console.log(nomi[i]);
+// }
 
-    if (arrayitemcilo % 2 !== 0){
-        console.log(arrayitemcilo);
-    }
-    
-}
-
-*/
+// versione while
 
 
-
-/*
-
-// Crea un array contenente 5 nomi e stampali a schermo
-
-
-var nomi = ["Mario", "Pippo", "Remo", "Pluto","Paperino"];
-
-
-for (var i = 0; i < nomi.length; i++){
+var i = 0;
+while (i < nomi.length){
+    //istruzione da eseguire
     console.log(nomi[i]);
+    // incremento - cambio della condizione per raggiungere l'uscita dal loop
+    i++;
 }
 
-*/
 
+// var i = 6;
 
-
-
-
-
-
-/*
-// Stampa tutti i numeri da 1000 a 0
-for (var i = 1000; i >=0; i--){
-    console.log(i);
-    
-}
-
-console.log("siamo usciti dal ciclo", "e il valore di i é: " + i);
+// do{
+//     //istruzione da eseguire
+//     console.log(nomi[i]);
+//     // incremento - cambio della condizione per raggiungere l'uscita dal loop
+//     i++;
+// } while (i < nomi.length);
 
 */
-
-
-/*
-// Stampa tutti i numeri da 0 a 1000
-
-for (var i = 0; i <=1000; i++){
-    console.log(i);  
-}
-
-console.log("siamo usciti dal ciclo", "e il valore di i é: " + i);
-
-
-// console.log(1);
-// console.log(2);
-// console.log(3);
-// ...
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Crea un array contenente 3 nomi. 
-// Chiedi un numero da 0 a 2 all’utente 
-// e stampa l’elemento dell’array corrispondente.
-
-/*
-var iscritti;
-
-// var iscritti = new Array();
-
-iscritti = ["Mario", "Filippo", "Giovanna", 1, ["arrayi nterno", 5, 4], 3, "stringa"];
-
-console.log("ouput di array con vari valori",iscritti[4][1]);
-
-
-// iscritti.push("Michele", "MARIANO");
-
-
-
-// MEGLIO NON FARLO PER NON CREARE BUCHI NELL'ARRAY
-// console.log(iscritti.length, iscritti);
-
-// iscritti[10] = "Simone";
-
-console.log("dopo ulteriore aggiunta", iscritti.length, iscritti);
-
-var lungarray = iscritti.length ;
-
-
-var num = parseInt(prompt("inserisci un numero tra 0 e " + lungarray));
-
-document.getElementById("mioid").innerHTML = iscritti[num];
-
-*/
-
-
-// if (num == 0) {
-//     document.getElementById("mioid").innerHTML = iscritti[0] ;
-// } else if (num == 1) {
-//     document.getElementById("mioid").innerHTML = iscritti[1] ;
-// } else {
-//     document.getElementById("mioid").innerHTML = iscritti[2] ;
-// }
-
-
-
-
-
-
-// Chiedi un numero all’utente e stampa in console il numero successivo.
-
-// var numero = parseInt(prompt("scrivi un numero"));
-
-// // var numplus = numero++;
-
-// document.getElementById("mioid").innerHTML = "il numero scelto è " + numero + " il numero successivo è " + (numero++);
-
-// console.log(numero);
