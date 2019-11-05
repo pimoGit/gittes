ffca5833b25d91151e80a87d4f44d7ad2edbfda0
Simone Icardi <info@simoneicardi.com>
2019-11-05 13:03:58 +0100

    intro jQuery

--- main.js
+++ main.js
@@ -1,66 +1,54 @@
-// Al click di un pulsante, deve apparire un alert con un numero random
-
-
-document.getElementById("mybutton").addEventListener("click",randnum);
-
-
-function randnum (){
-    var num = Math.floor((Math.random() * 100) + 1);
-    alert(num);
-}
-
-
-
-/*
-// Ciao Visitatore!
-// Imposta il nome di colore rosso in vari modi:
-// ● Solo con HTML+CSS
-// ● Aggiungendo la classe con JS
-// ● Scrivendo su element.style
-
-var el = document.getElementById("messaggio");
-
-// Aggiungendo la classe con JS
-// el.className = el.classList + " rosso";
-
-// Scrivendo su element.style
-el.style.color = "red";
-*/
-
-/*
-// Simulare un countdown di 10 secondi che alla fine dice buon anno
-
-var secondi = 10;
-
-var myinterval = setInterval(countdown, 500);
-
-
-function countdown(){
-
-    secondi = secondi - 1;
+$( document ).ready(function() {
     
-    if(secondi == 0){
-        console.log("Buon Annoooo!!!"); 
-        clearInterval(myinterval);
-    } else {
-        console.log(secondi);
-    }
+    // Creare il titolo di un paragrafo e un bottone che permette di espandere 
+    // il testo inizialmente nascosto
 
-}
+    var elem = $(".accordion");
+
+        // elem.hide();
+
+        $("#controller").click(function(){
+            // elem.show(2000);
+            elem.addClass("visible");
+        });
+
+
+
+
+
+    // Creare un bottone che on hover aggiunga una classe che cambia il colore del testo e la toglie quando il mouse esce dal bottone.
+    // Con il doppio click cambio il testo in “cliccato”. Con un click solo non fa niente
+
+
+    /*
+    // $("#messaggio").show(2000);
+
+    $("#mybutton").on({
+        mouseenter: function() {
+            $( "p" ).addClass( "red" ); },
+        mouseleave: function() {
+            $( "p" ).removeClass( "red" ); },
+        dblclick: function(){
+            $("#mybutton").text("cliccato"); 
+            $("#messaggio").hide(2000);
+            // $("#messaggio").fadeOut();
+        }
+
+
+    });
+*/
+    
+
+
+    /*
+    $("div.miaclasse").addClass("nuovaclasse");
+
+    $("p:first").addClass("paragraphclasse");
 */
 
 
-/*
-// Il programma chiede all’utente quanti secondi mancano alla cottura della pasta.
-// Dopo il tempo indicato, appare un alert()
-
-var tempo = parseInt(prompt("quanti secondi mancano alla cottura?"));
-
-setTimeout(pasta, tempo*1000);
 
 
 
- function pasta (){
-    alert("la pasta è prontaaaaa!");
-}
-*/
+
+});
