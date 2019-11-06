c7df12e7e335d50130afd9fc9d71608f36bda13e
Simone Icardi <info@simoneicardi.com>
2019-11-06 12:58:45 +0100

    jQuery step 2 +this, live coding

--- main.js
+++ main.js
@@ -1,18 +1,101 @@
 $( document ).ready(function() {
+
+
+    // Creare una To do List. On click su un item verrà cancellata la riga. 
+    // Una volta che l’item è nascosto, far comparire un modal: 
+    // “Item Cancellato” e farlo scomparire subito dopo.
+
+
     
+    // On click su un item verrà cancellata la riga.
+    $(".delete").click(function(){
+        $(this).parent("li").hide(1000,function(){
+            // $("#modal").fadeIn(1000, function(){
+            //     $(this).fadeOut();
+            // });
+            $("#modal").fadeIn(1000).fadeOut();
+        //         $("#modal").fadeIn(1000);
+        //         setTimeout(function(){
+        //             $("#modal").fadeOut();
+        //         },2000);
+        //     });
+
+        });
+    });
+
+
+
+    
+/*
+    // Creare una piccola sezione FAQ. 
+    // Tante domande che quando cliccate visualizzano la risposta corrispondente.
+
+    $(".question").click(function(){
+        // $(this).find("p").toggle(200);
+        $(this).children("p").toggle(200);
+    });
+
+*/
+
+    /*
+    $(".paragrafo").click(function(){
+        $(this).hide();
+    })
+    */
+
+    /*
+    var ancorAttr = $("a").attr("href");
+    $("a").attr("href", "http://www.yahoo.com");
+    console.log("url del link: ",ancorAttr);
+
+    var mioattr = $("a").attr("data-status");
+    console.log("il mio atttributo inventato ha vaklore: ", mioattr);
+    
+
+
+    $("#controller").text("testo inserito da metodo");
+
+    var textel = $("#controller").text();
+
+    console.log(textel);
+    
+
+
+    // Creare un bottone che faccia aprire e chiudere un sipario.
+
+    $(".butsipario").click(function(){
+        $(".sipario").slideToggle(3000, callbackfunct);
+    });
+
+    function callbackfunct(){
+        alert("fine animazione");         
+    }
+*/
+
+    /*
     // Creare il titolo di un paragrafo e un bottone che permette di espandere 
     // il testo inizialmente nascosto
 
     var elem = $(".accordion");
 
+    // var statoElemento = "chiuso";
+
         // elem.hide();
 
         $("#controller").click(function(){
             // elem.show(2000);
-            elem.addClass("visible");
+            // if(statoElemento === "chiuso"){
+            //     elem.addClass("visible");
+            //     statoElemento = "aperto";
+            // } else {
+            //     elem.removeClass("visible");
+            //     statoElemento = "chiuso";
+            // }
+            elem.toggleClass("visible");
+            
         });
 
-
+*/
 
 
 
