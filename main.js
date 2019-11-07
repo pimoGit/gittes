e01a523c7d733c77762d2fac35b7a2c27bd153ac
Simone Icardi <info@simoneicardi.com>
2019-11-07 13:17:55 +0100

    real world example, slider basic coding

--- main.js
+++ main.js
@@ -1,137 +1,63 @@
 $( document ).ready(function() {
 
 
-    // Creare una To do List. On click su un item verrà cancellata la riga. 
-    // Una volta che l’item è nascosto, far comparire un modal: 
-    // “Item Cancellato” e farlo scomparire subito dopo.
+    $(".next").click(prossimaslide);
 
-
-    
-    // On click su un item verrà cancellata la riga.
-    $(".delete").click(function(){
-        $(this).parent("li").hide(1000,function(){
-            // $("#modal").fadeIn(1000, function(){
-            //     $(this).fadeOut();
-            // });
-            $("#modal").fadeIn(1000).fadeOut();
-        //         $("#modal").fadeIn(1000);
-        //         setTimeout(function(){
-        //             $("#modal").fadeOut();
-        //         },2000);
-        //     });
-
-        });
-    });
+    $(".prev").click(precedenteslide);
 
 
 
-    
-/*
-    // Creare una piccola sezione FAQ. 
-    // Tante domande che quando cliccate visualizzano la risposta corrispondente.
 
-    $(".question").click(function(){
-        // $(this).find("p").toggle(200);
-        $(this).children("p").toggle(200);
-    });
-
-*/
-
-    /*
-    $(".paragrafo").click(function(){
-        $(this).hide();
-    })
-    */
-
-    /*
-    var ancorAttr = $("a").attr("href");
-    $("a").attr("href", "http://www.yahoo.com");
-    console.log("url del link: ",ancorAttr);
-
-    var mioattr = $("a").attr("data-status");
-    console.log("il mio atttributo inventato ha vaklore: ", mioattr);
-    
-
-
-    $("#controller").text("testo inserito da metodo");
-
-    var textel = $("#controller").text();
-
-    console.log(textel);
-    
-
-
-    // Creare un bottone che faccia aprire e chiudere un sipario.
-
-    $(".butsipario").click(function(){
-        $(".sipario").slideToggle(3000, callbackfunct);
-    });
-
-    function callbackfunct(){
-        alert("fine animazione");         
-    }
-*/
-
-    /*
-    // Creare il titolo di un paragrafo e un bottone che permette di espandere 
-    // il testo inizialmente nascosto
-
-    var elem = $(".accordion");
-
-    // var statoElemento = "chiuso";
-
-        // elem.hide();
-
-        $("#controller").click(function(){
-            // elem.show(2000);
-            // if(statoElemento === "chiuso"){
-            //     elem.addClass("visible");
-            //     statoElemento = "aperto";
-            // } else {
-            //     elem.removeClass("visible");
-            //     statoElemento = "chiuso";
-            // }
-            elem.toggleClass("visible");
+    // funzioni
+    function prossimaslide(){
             
-        });
+        var posizioneimg = $(".slider-wrapper .images  img.active");
+        var posizionenav = $(".nav i.active");
 
-*/
-
-
-
-    // Creare un bottone che on hover aggiunga una classe che cambia il colore del testo e la toglie quando il mouse esce dal bottone.
-    // Con il doppio click cambio il testo in “cliccato”. Con un click solo non fa niente
+        posizioneimg.removeClass("active");
+        posizionenav.removeClass("active");
 
 
-    /*
-    // $("#messaggio").show(2000);
-
-    $("#mybutton").on({
-        mouseenter: function() {
-            $( "p" ).addClass( "red" ); },
-        mouseleave: function() {
-            $( "p" ).removeClass( "red" ); },
-        dblclick: function(){
-            $("#mybutton").text("cliccato"); 
-            $("#messaggio").hide(2000);
-            // $("#messaggio").fadeOut();
+        // se siamo all'utima img dello slider, ritorna ad essere attiva la prima
+        if(posizioneimg.hasClass("last")){
+            $(".slider-wrapper .images  img.first").addClass("active");
+            $(".nav i.first").addClass("active");
+        } else{
+            //altrimenti passa semplicemente l'active alla prossima
+            posizioneimg.next("img").addClass("active");
+            posizionenav.next("i").addClass("active");
         }
 
-
-    });
-*/
-    
+        
+    }
 
 
-    /*
-    $("div.miaclasse").addClass("nuovaclasse");
+    function precedenteslide(){
+            
+        var posizioneimg = $(".slider-wrapper .images  img.active");
+        var posizionenav = $(".nav i.active");
 
-    $("p:first").addClass("paragraphclasse");
-*/
+        posizioneimg.removeClass("active");
+        posizionenav.removeClass("active");
 
+        // se siamo alla prima img dello slider, ritorna ad essere attiva l'ultima
+        if(posizioneimg.hasClass("first")){
+            $(".slider-wrapper .images  img.last").addClass("active");
+            $(".nav i.last").addClass("active");
+        } else{
+            //altrimenti passa semplicemente l'active alla prossima
+            posizioneimg.prev("img").addClass("active");
+            posizionenav.prev("i").addClass("active");
+        }
 
-
-
+        
+    }
 
 
 });
+
+
+
+
+
+
