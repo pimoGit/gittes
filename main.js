5a981aaa6b03d8745ed5ff58e40fd40cefbb97a9
Simone Icardi <info@simoneicardi.com>
2019-11-18 13:25:34 +0100

    Ajax and API intro

--- main.js
+++ main.js
@@ -1,50 +1,27 @@
 $( document ).ready(function() {
 
-        //agganciamo al click sul bottone la funzione di callbnack
-        $(".sendmsg").click(function(){
+        var elemento = $("#numrandom");
 
-        //ci salviamo il valore dell'input inserito dall'utente
-        var messaggio = $(".message").val();
-        // console.log(messaggio);
+        $("#butn").click(function () {
+                // var mioele = $(this);
+                $.ajax({
+                        url: "https://flynn.boolean.careers/exercises/api/random/word ",
+                        method: "GET",
+                        success: function (datiritorno){
+                                alert("hey è andata tutto bene!")
+                                console.log(datiritorno);
+                                elemento.html(datiritorno.response);
+                                
+                        },
+                        error: function (richiesta, stato){
+                                console.log("c'è stato un errore: " + stato);
+                                
+                        }
 
-        // $("#container").append(
-        //         "<div class='msgsent'>" + messaggio +"</div>"
-        // );
+                });
+
+});
         
-        // cloniamo (facciamo una copia) del div con classe "msgsent" che sta dentro
-        // un div con ID "template"
-        // var elmentmsg = $("#template .msgsent").clone();
-        // console.log(elmentmsg);
-
-        // var source   = document.getElementById("hb-template").innerHTML;
-        var sorgente   = $("#hb-template").html();
-        console.log(sorgente);
-
-        var sorgDigerita = Handlebars.compile(sorgente);
-
-
-
-        // modifica questa copia di "msgsent" aggiungendogli il testo del messaggio
-        // elmentmsg.find(".testo").text(messaggio);
-        
-
-        var objRef = {text: messaggio, orario: "10:15", classname: "msgsent"};
-
-        var elValorizzato    = sorgDigerita(objRef);
-
-        
-        // appendiamo una copia con testo valorizzato del div "msgsent"
-        // $("#container").append(elmentmsg);
-        $("#container").append(elValorizzato);
-
-        // ripuliamo il contenuto dell'input, per UX
-        $(".message").val("");
-
-
-        });
-
-        
-
 
 
 });
