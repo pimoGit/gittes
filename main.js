091d4ecec152c4152fd9e30e96b9e5ce2dedd18a
Simone Icardi <info@simoneicardi.com>
2019-11-21 12:54:09 +0100

    ex calendar first part live coding

--- main.js
+++ main.js
@@ -1,27 +1,61 @@
 $( document ).ready(function() {
 
-        var elemento = $("#numrandom");
+        //Ottengo il numero di giorni di quello specifico mese 
+        var datadefinita = moment('01/01/2018', 'DD/MM/YYYY');
+        var numeroDiGiorni = datadefinita.daysInMonth();
+        // console.log("tipo numero giorni" ,typeof numeroDiGiorni);
+        
+        
+        //Eseguo un ciclo per creare ciascun giorno del mese nel mio html
+        for(var i = 1; i <= numeroDiGiorni; i++) {
+        
+                //Strutturo la data simile alla chiamata ajax
+                var currentDate = moment('2018-01-'+i, 'YYYY-MM-D').format('YYYY-MM-DD');
+                
+                //Uso quella data per prendere le informazioni di quel giorno : numero e nome del giorno
+                var currentDay = moment(currentDate).format('DD dddd');
+                
+                //Inserisco nell'html il mio div con attributo per eventuali selettori e la relativa data formattata
+                $('.content').append('<div data-date="'+currentDate+'">'+currentDay+'</div>')
+    }
+        
 
-        $("#butn").click(function () {
-                // var mioele = $(this);
-                $.ajax({
-                        url: "https://flynn.boolean.careers/exercises/api/random/word ",
-                        method: "GET",
-                        success: function (datiritorno){
-                                alert("hey è andata tutto bene!")
-                                console.log(datiritorno);
-                                elemento.html(datiritorno.response);
-                                
-                        },
-                        error: function (richiesta, stato){
-                                console.log("c'è stato un errore: " + stato);
-                                
+        //     interrogiamo la API per farci ritornare le festività del mese
+
+        $.ajax({
+                url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+                method: "GET",
+                success: function(data){
+                        // console.log(data);
+                        var arrdata = data.response;
+                        console.log(arrdata);
+
+                        for (var i = 0; i < arrdata.length; i++){
+                          var oggetto = arrdata[i];
+
+                          var nomefesta = oggetto.name;
+                          var datafesta = oggetto.date;
+
+                        //   console.log(nomefesta, datafesta);
+
+                          var elementoSelez = $("div[data-date='" + datafesta + "']");
+                          console.log(elementoSelez);
+
+                          if(elementoSelez){
+                                elementoSelez.addClass("festa").append(" " + nomefesta);
+                          }
+
+
                         }
 
-                });
+                },
+                error: function(){
+                        console.log("c'è stato un errarore!");
+                }
+        }
 
-});
-        
+        )
+
 
 
 });
