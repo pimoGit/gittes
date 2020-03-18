1b086887a1eee794501b86fb62d3a43cf32183c8
Simone <info@simoneicardi.com>
2020-03-18 10:46:42 +0100

    LC intro var js

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 // document.writeln('Hello World!');
 //  alert('Hello World!');
+
+// dichiarazione variabile
+var miaStringa;
+
+alert(miaStringa);
+
+// assegnazione primo valore alla var
+miaStringa = 'ciao';
+// utilizzo e richiamo del valore associato al momento alla var
+// alert(miaStringa);
+console.log("la variabile a linea 13 vale", miaStringa);
+
+// riassegno nuovo valore a var
+miaStringa = 'arrivederci';
+// richiamo nuovamente valore var (cambiato)
+alert(miaStringa);
+
 document.getElementById('mio_id').innerHTML='Hello World!';
 console.log("hey io sappaio in colnsole!");
