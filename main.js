ff05477f0529212afb5940527ca5c13d14076357
Simone <info@simoneicardi.com>
2020-03-18 12:12:21 +0100

    LC saluto uso di log per debug

--- main.js
+++ main.js
@@ -13,4 +13,6 @@ nomeUtente = prompt("inserisci il tuo nome");
 // salutare il visitatore aggiungendo il suo nome
 document.getElementById('mio_id').innerHTML = "Ciao " + nomeUtente;
 
+nomeUtente = "Olmo";
+
 console.log(" in realtà caro" +  nomeUtente + "sei un pò str..");
