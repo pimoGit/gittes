9c2bda9150fee27299c290439f8ba5bc284a536c
Simone <info@simoneicardi.com>
2020-03-23 10:37:41 +0100

    LC intro array

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
-// Gioco: pari o dispari?
+// Crea un array contenente 3 nomi.
+// Chiedi un numero da 0 a 2 all’utente e stampa l’elemento
+// dell’array corrispondente.
 
-var result, msg;
+var mioArray = ["Mario","Francesca","Lorella"];
+var miaStringa = "Hello";
 
-// chiediamo all'utente di scegliere tra pari e dispari
-var userChoice = prompt("scegli se puntare su pari o dispari");
+var numArray = [1,"strainga",5,true,miaStringa];
 
-// chiediamo all'utente di fare la sua puntatata (numero tra 1-5)
-var userNum = parseInt(prompt("scegli un numero tra 1 e 5"));
-
-// pc punta numero (numero tra 1-5)
-var pcNum = Math.floor(Math.random() * 5) + 1;
-
-// faccio somma dei 2 numeri
-var sum = userNum + pcNum;
-
-console.log("numero utente= " +  userNum);
-console.log("numero PC= " +  pcNum);
-console.log("somma= " +  sum);
+var indice = parseInt(prompt("inserisci un numero da 0 a 2"));
 
 
-// cerco di capire se la somma sia pari o dispari (mi salvo la cosa)
-if (sum % 2 === 0){
-  result = "pari";
-} else {
-  result = "dispari";
-}
+// prova varia di aggiunta, ritorno lunghezza
+console.log(mioArray);
+console.log("lunghezza iniziale", mioArray.length);
 
-// cerco di capire sulla base della valore di pari o disp di somma se ha vinto l'untete o il pc
-if (result === userChoice ){
-  msg = "Ha vinto lo user";
-} else if ( result !== userChoice){
-  msg = "Ha vinto il PC";
-}
+var lunghezzaArr = mioArray.push("Gianfranco");
+
+console.log("Il valore ritornato dal push è: ", lunghezzaArr);
+
+console.log("lunghezza successiva al push", mioArray.length);
+
+console.log(mioArray);
+
+mioArray[mioArray.length] = "Simone";
+
+console.log("lunghezza successiva a inserimento manuale", mioArray.length);
+
+console.log(mioArray);
+
+mioArray[0] = "nomneCambiato";
+
+console.log("lunghezza successiva a inserimento manuale", mioArray.length);
+
+console.log(mioArray);
 
 
 
-// dò output di chi ha vinto
-document.getElementById('mio_id').innerHTML = msg;
+
+
+var itemTrovato = mioArray[indice];
+
+document.getElementById('mio_id').innerHTML = itemTrovato;
