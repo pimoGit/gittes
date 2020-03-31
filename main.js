8594de1b5775ab36b91729f09d87ad13e479cd03
Simone <info@simoneicardi.com>
2020-03-31 12:33:12 +0200

    LC event managing jQuery

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-// Data una struttura html selezionare gli elementi indicati e
-// applicare loro una class con addClass
-
-var titolo = $('h1');
-
-var par = $('p, p.pclass');
-
-console.log(titolo);
-
-titolo.addClass('classToAdd');
-
-par.addClass('classToAdd');
+// Creare un bottone che on hover aggiunga una classe
+// che cambia il colore del testo
+// e la toglie quando il mouse esce dal bottone.
+// Con il doppio click cambio il testo in “cliccato”.
+// Con un click solo non fa niente
 
 
-$('#divId').addClass('classToAdd');
+var bottone =  $('.bottone');
+
+bottone.on({
+  mouseenter: function(){
+      bottone.addClass('classToAdd');
+  },
+  mouseleave: function(){
+      bottone.removeClass('classToAdd');
+  },
+  dblclick: function(){
+      bottone.text('cliccato');
+  }
+});
+
+// bottone.mouseenter(
+//     function(){
+//       bottone.addClass('classToAdd');
+//     }
+//   );
+//   bottone.mouseleave(
+//       function(){
+//         bottone.removeClass('classToAdd');
+//       }
+//     );
