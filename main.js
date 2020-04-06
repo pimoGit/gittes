a224d1d3623c97b897cc1cdea954c3fb9b808a39
Simone <info@simoneicardi.com>
2020-04-06 10:35:52 +0200

    LC setTimout use case

--- main.js
+++ main.js
@@ -1,53 +1,20 @@
-// Creare in HTML una griglia formata da 8x8 rettangolini tutti bianchi.
-// 15 di questi rettangolini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi
-
+// Il programma chiede all’utente quanti secondi mancano alla cottura della pasta.
+// Dopo il tempo indicato, appare un alert()
 
 $(document).ready(function(){
 
-  // tenere il punteggio dei Verdi VS Rossi
-var redPoint = 0, greenPoint = 0;
-
-  // faccio cose
-  // $('.square').click(
-  //   function () {
-  //     if($(this).hasClass('redWannabe')){
-  //       $(this).css('background', 'red');
-  //       redPoint++;
-  //       console.log("punteggio rosso: " + redPoint);
-  //       $('.prossa').html("punteggio rosso: " + redPoint);
-  //     } else {
-  //       $(this).css('background', 'green');
-  //       greenPoint++;
-  //       console.log("punteggio verde: " + greenPoint);
-  //       $('.pverde').html("punteggio verde: " + greenPoint);
-  //     }
-  //   }
-  // );
+  var tempo = parseInt(prompt("inserisci i secondi mancanti"));
 
 
-  $('.square').click(
-    function () {
-      if($(this).hasClass('active')){
-        console.log("OH! l'hai già cliccato!");
-      } else if($(this).hasClass('redWannabe')){
-        $(this).css('background', 'red');
-        redPoint++;
-        $(this).addClass('active');
-        // $(this).attr('active', 'vero');
-        console.log("punteggio rosso: " + redPoint);
-        $('.prossa').html("punteggio rosso: " + redPoint);
-      } else {
-        $(this).css('background', 'green');
-        greenPoint++;
-        $(this).addClass('active');
-        console.log("punteggio verde: " + greenPoint);
-        $('.pverde').html("punteggio verde: " + greenPoint);
-      }
-    }
-  );
+  setTimeout(pasta, tempo * 1000);
 
 
 
+
+  function pasta() {
+    // alert("la pasta è pronta!");
+    $('h1').html("la pasta è pronta!");
+  }
 
 
 });
