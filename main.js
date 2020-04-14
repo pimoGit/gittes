09f91d81b5a5e0e087e5014544bbd114cfc4d681
Simone <info@simoneicardi.com>
2020-04-14 10:53:49 +0200

    LC milestone 3 BoolzApp

--- main.js
+++ main.js
@@ -5,19 +5,23 @@ $(document).ready(function(){
   var chatWin = $('.right-messages');
   var inputMsg = $('.new-message-inputs');
   var buttonSend = $('.right-footer-icon.f-right');
+  var contatti = $('.contact');
 
   //gestisco evento su bottone di invio
   buttonSend.click(
     function () {
+      // è possibile inserire nuovi messaggi per ogni conversazione [attiva]
+      var chatWinActive = $('.right-messages.active');
+
       var msg = inputMsg.val();
       // console.log(msg);
-      chatWin.append('<div class="message sent"><span class="message-text">' + msg + '</span><i class="fa fa-chevron-down f-right message-options"></i><span class="message-time">23:26</span></div>');
+      chatWinActive.append('<div class="message sent"><span class="message-text">' + msg + '</span><i class="fa fa-chevron-down f-right message-options"></i><span class="message-time">23:26</span><div class="message-options-panel"><div class="message-destroy">Cancella messaggio</div></div></div>');
       inputMsg.val("");
 
       //dopo un secondo
       setTimeout(
         function(){
-          chatWin.append('<div class="message received"><span class="message-text">OK!</span><i class="fa fa-chevron-down f-right message-options"></i><span class="message-time">23:26</span></div>');
+          chatWinActive.append('<div class="message received"><span class="message-text">OK!</span><i class="fa fa-chevron-down f-right message-options"></i><span class="message-time">23:26</span><div class="message-options-panel"><div class="message-destroy">Cancella messaggio</div></div></div>');
         }
         ,1000);
       // deve apparire un nuovo msg con un testo sempre uguale (statico)
@@ -35,7 +39,7 @@ $(document).ready(function(){
 
 
     // selezionare tutti i blocchi di contatto e ciclare tra di essi (each())
-    $('.contact').each(
+    contatti.each(
       function () {
       //salvo in una var il valore del testo del nome nel contatto (stringa2)
       var stringaNome = $(this).find('.contact-name').text().toLowerCase();
@@ -54,22 +58,52 @@ $(document).ready(function(){
 
 
   //Click sul contatto mostra la conversazione del contatto cliccato
+  contatti.click(
     // click sul contatto che ha data-attr che corrisponde a stesso data-attr in chat
-    // salvo il valore dell'attr e lo usso per dire quale chat è attiva
+
+    function () {
+      contatti.removeClass("active");
+      $(this).addClass("active");
+      // salvo il valore dell'attr e lo usso per dire quale chat è attiva
+      var dataAttr = $(this).data("conversazione");
+      // console.log(dataAttr);
+      chatWin.removeClass("active");
+      $('.right-messages[data-conversazione="' + dataAttr + '"]').addClass("active");
+    }
+  );
 
 
-  // è possibile inserire nuovi messaggi per ogni conversazione [attiva]
 
 
-  // Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+  // gestisci i sottomenù di cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
     // son riuascito ad agganciarte l'evento sul "delete" potrò dirgli una roba tipo this.padre.cancella();
-    $('.right-messages').on("click", ".message",
+    $('.right-messages').on("click", ".message-options",
        function () {
-      //   alert("hai cliccato su di me!");
-        $(this).hide();
+
+        // ref a blocchetto msg padre della freccia su cui è registrato il click
+        var msgInst = $(this).closest('.message');
+
+        // sui fratelli del msg individuato, rendi non visibilio i sottomenù
+        msgInst.siblings('.message').find('.message-options-panel').removeClass("active");
+
+        msgInst.find('.message-options-panel').toggleClass("active");
        }
 
     );
+
+    // Cancella messaggio sul click del relativo "bottone" di delete
+    $('.right-messages').on("click", ".message-destroy",
+       function () {
+         // alert("hai cliccato su destroy");
+         // ref a blocchetto msg padre della freccia su cui è registrato il click
+         var msgInst = $(this).closest('.message');
+         msgInst.remove();
+
+       }
+
+    );
+
+
 
 
 });
