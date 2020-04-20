6405a815899a1c46ef5f0d429a776a8ad77b82e5
Simone <info@simoneicardi.com>
2020-04-20 10:00:15 +0200

    LC griglia ajax

--- main.js
+++ main.js
@@ -2,30 +2,35 @@
 $(document).ready(function(){
 
   var numRandom;
+
 //gestione evento
-  //var self = $(this);
+$('.square').click( function () {
 
-  $.ajax({
-      url : "https://flynn.boolean.careers/exercises/api/random/int",
-      method : "GET",
-      success : function (data,stato) {
-        // $("#risultati").html(data);
-        console.log(data.response, data.success);
-        numRandom = data.response;
-        if(numRandom < 5){
-          $('h1').addClass("rosso");
-        } else {
-          $('h1').addClass("giallo");
+  var self = $(this);
+
+    $.ajax({
+        url : "https://flynn.boolean.careers/exercises/api/random/int",
+        method : "GET",
+        success : function (data,stato) {
+          // $("#risultati").html(data);
+          // console.log(data.response, data.success);
+          numRandom = data.response;
+          if(numRandom <= 5){
+            self.addClass("giallo");
+          } else {
+            self.addClass("verde");
+          }
+          self.text(numRandom);
+        },
+        error : function (richiesta,stato,errori) {
+          console.log("E' avvenuto un errore. " + errori, "stato " + stato, richiesta);
         }
-        $('h1').text(numRandom);
-      },
-      error : function (richiesta,stato,errori) {
-        console.log("E' avvenuto un errore. " + errori, "stato " + stato, richiesta);
-      }
+    });
+
+    // non possibile perchè chiamata è asincrona
+    console.log("la var definita è: ", numRandom);
+
+
   });
-
-  // non possibile perchè chiamata è asincrona
-  console.log("la var definita è: ", numRandom);
-
 
 });
