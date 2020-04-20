6b85d7d4994ee500349452593f72044d43bfd86a
Simone <info@simoneicardi.com>
2020-04-20 13:07:02 +0200

    LC ex dischi musicali

--- main.js
+++ main.js
@@ -1,36 +1,42 @@
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
+// Servendoci di handlebars stampiamo tutto a schermo. In questo momento non è importante la parte grafica.
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-  var numRandom;
-
-//gestione evento
-$('.square').click( function () {
-
-  var self = $(this);
-
-    $.ajax({
-        url : "https://flynn.boolean.careers/exercises/api/random/int",
-        method : "GET",
-        success : function (data,stato) {
-          // $("#risultati").html(data);
-          // console.log(data.response, data.success);
-          numRandom = data.response;
-          if(numRandom <= 5){
-            self.addClass("giallo");
-          } else {
-            self.addClass("verde");
-          }
-          self.text(numRandom);
-        },
-        error : function (richiesta,stato,errori) {
-          console.log("E' avvenuto un errore. " + errori, "stato " + stato, richiesta);
-        }
-    });
-
-    // non possibile perchè chiamata è asincrona
-    console.log("la var definita è: ", numRandom);
+  var source = $('.album-template').html();
+  var template = Handlebars.compile(source);
 
 
+  $.ajax({
+    url: "https://flynn.boolean.careers/exercises/api/array/music",
+    method: "GET",
+    success: function(data,stato) {
+      var album = data.response;
+      console.log(album);
+      for (var i = 0; i < album.length; i++) {
+        var context = {
+          cover: album[i].poster,
+          title: album[i].title,
+          artist: album[i].author,
+          year: album[i].year
+        };
+        var html = template(context);
+        $('.box').append(html);
+      }
+    },
+    error: function(richiesta,stato,errore){
+      alert("Chiamata fallita!!!");
+    }
   });
+
+
+
+
+
+
+
+
+
+
 
 });
