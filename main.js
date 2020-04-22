85ebdabab84f6f4d91e2dda8190680c1a3ee78d3
Simone <info@simoneicardi.com>
2020-04-22 10:52:23 +0200

    LC Boolflix milestone1

--- main.js
+++ main.js
@@ -3,46 +3,76 @@
 
 $(document).ready(function () {
 
-  var source = $('.album-template').html();
+  var contPage = $('.box');
+
+  // inizializzazione template handlebars
+  var source = $('#movie-template').html();
   var template = Handlebars.compile(source);
 
   // salvo input utente
   //var variabileName = //quello che devo salvare;
 
-  $.ajax({
-    url: "https://flynn.boolean.careers/exercises/api/array/music",
-    method: "GET",
-    // utilizzo data object per aggiungere query a request API
-    // data: {
-    //   api_key: diuewghfiugwefkjgewkf,
-    //   language: "it-IT",
-    //   query: variabileName
-    // },
-    success: function(data,stato) {
-      var album = data.response;
-      console.log(album);
-      for (var i = 0; i < album.length; i++) {
-        var context = {
-          cover: album[i].poster,
-          title: album[i].title,
-          artist: album[i].author,
-          year: album[i].year
-        };
-        var html = template(context);
-        $('.box').append(html);
+  $('#invioQuery').click(function () {
+
+    // reset di pagina
+    contPage.html("");
+
+    var stringaRichiesta = $('#queryString').val();
+
+    // console.log(stringaRichiesta);
+
+
+    $.ajax({
+      url: "https://api.themoviedb.org/3/search/movie",
+      method: "GET",
+      // utilizzo data object per aggiungere query a request API
+      data: {
+        api_key: "e99307154c6dfb0b4750f6603256716d",
+        language: "it-IT",
+        query: stringaRichiesta
+      },
+      success: function(data,stato) {
+        // console.log(data);
+        var movieList = data.results;
+        console.log(movieList);
+
+        generaOutput(movieList);
+
+      },
+      error: function(richiesta,stato,errore){
+        alert("Chiamata fallita!!!");
       }
-    },
-    error: function(richiesta,stato,errore){
-      alert("Chiamata fallita!!!");
-    }
+    });
+
+
+
   });
 
 
 
 
 
+ // FUNZIONI
 
+ function generaOutput(listaOggetti) {
 
+   for (var i = 0; i < listaOggetti.length; i++) {
+     var movie = listaOggetti[i];
+
+     var context = {
+       titolo: movie.title,
+       titoloOriginale: movie.original_title,
+       lingua: movie.original_language,
+       voto: movie.vote_average
+     };
+
+     var html = template(context);
+
+     contPage.append(html);
+
+   }
+
+ }
 
 
 
