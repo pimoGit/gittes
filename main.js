85b6c3c79b850eeb528e322ec3923b0a22a261cc
Simone <info@simoneicardi.com>
2020-04-23 11:23:05 +0200

    LC Boolflix milestone2

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
 
     // console.log(stringaRichiesta);
 
-
+    // richiesta film
     $.ajax({
       url: "https://api.themoviedb.org/3/search/movie",
       method: "GET",
@@ -36,7 +36,30 @@ $(document).ready(function () {
         var movieList = data.results;
         console.log(movieList);
 
-        generaOutput(movieList);
+        generaOutput(movieList, "film");
+
+      },
+      error: function(richiesta,stato,errore){
+        alert("Chiamata fallita!!!");
+      }
+    });
+
+    // richiesta serie TV
+    $.ajax({
+      url: "https://api.themoviedb.org/3/search/tv",
+      method: "GET",
+      // utilizzo data object per aggiungere query a request API
+      data: {
+        api_key: "e99307154c6dfb0b4750f6603256716d",
+        language: "it-IT",
+        query: stringaRichiesta
+      },
+      success: function(data,stato) {
+        // console.log(data);
+        var movieList = data.results;
+        console.log(movieList);
+
+        generaOutput(movieList, "tv");
 
       },
       error: function(richiesta,stato,errore){
@@ -54,21 +77,35 @@ $(document).ready(function () {
 
  // FUNZIONI
 
- function generaOutput(listaOggetti) {
+ // funzione di output generale
+ function generaOutput(listaOggetti, tipo) {
 
    for (var i = 0; i < listaOggetti.length; i++) {
      var movie = listaOggetti[i];
 
+     var title, orgiginalTitle;
+
      // se tipo è movie
-     // allora var titoloGenerato = movie.title
-     // se tipo è tv
-     // var titoloGenerato = movie.name
+     if(tipo === "film"){
+       // allora var titoloGenerato = movie.title
+       title = movie.title;
+       orgiginalTitle = movie.original_title;
+
+    // se tipo è tv
+     } else if (tipo === "tv"){
+       title = movie.name;
+       orgiginalTitle = movie.original_name;
+     }
+
+
+
 
      var context = {
-       titolo: movie.title,
-       titoloOriginale: movie.original_title,
-       lingua: movie.original_language,
-       voto: movie.vote_average
+       titolo: title,
+       titoloOriginale: orgiginalTitle,
+       lingua: flagGenerator(movie.original_language),
+       voto: votoStelle(movie.vote_average),
+       tipoRichiesta: tipo
      };
 
      var html = template(context);
@@ -78,6 +115,39 @@ $(document).ready(function () {
    }
 
  }
+
+  // funzione di output voto in stelline
+  function votoStelle(voto) {
+    // passo da voto base 10 a voto base 5
+    var votoBase5 = Math.ceil(voto/2);
+
+    var stars = "";
+
+    for (var i = 1; i <= 5; i++) {
+        if(i <= votoBase5){
+        // <!-- stella piena -->
+  			stars += '<i class="fas fa-star"></i>';
+      } else {
+        // <!-- stella vuota -->
+  			stars += '<i class="far fa-star"></i>';
+        // stars = stars + '<i class="far fa-star"></i>';
+      }
+    }
+    return stars;
+  }
+
+  // funzione di output bandierine
+  function flagGenerator(codiceLang) {
+    // lista imgs presenti in cartella img
+    var images =  ["it", "en"];
+    var imgGenerata;
+
+    if(images.includes(codiceLang)){
+      imgGenerata = '<img src="img/' + codiceLang + '.svg" alt="immagine" class="flags" >';
+      return imgGenerata;
+    }
+    return codiceLang;
+  }
 
 
 
