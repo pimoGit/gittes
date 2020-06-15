836b8dca469ff6bd8c5a8a8a1aab0d6ed945c6bd
Simone <info@simoneicardi.com>
2020-06-15 12:41:53 +0200

    LC gioco pari o dispari

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
-// Inserisci due parole, quale è la piu lunga?
+// Gioco: pari o dispari? tra user e cpu
 
-// dichiarazione variabili varie
-var parolaPiuLunga;
+var risultato;
 
-// l'utente inserisce 2 parole di input
-var stringa1 = prompt("inserisci la prima parola");
-var stringa2 = prompt("inserisci la seconda parola");
+// chiediamo la scommessa all'utente [pari/dispari]
+var scommessa = prompt("cosa uscirà, pari o dispari?");
+console.log(scommessa);
+// chiediamo puntata del numero
+var numeroUtente = parseInt(prompt("inserisci un numero tra 1 e 5"), 10);
+console.log(numeroUtente);
+// var numeroUtenteNumero = parseInt(numeroUtente);
+// console.log("numero virato utente", numeroUtenteNumero);
+// la cpu sceglie un numero
+var numeroCpu = Math.floor(Math.random() * 5) + 1;
+console.log(numeroCpu);
 
-console.log(stringa1, stringa2);
+// faccio somma dei due numeri
+var somma = numeroUtente + numeroCpu;
+console.log(somma);
 
-var lunghezzaStringa1 = stringa1.length;
-var lunghezzaStringa2 = stringa2.length;
-
-console.log(stringa1 + "è lunga: " + lunghezzaStringa1, stringa2 + "è lunga: " + lunghezzaStringa2);
-
-
-
-// verifico quale delle 2 è più lunga
-  // se è più lunga la prima comunico la cosa
-if ( lunghezzaStringa1 > lunghezzaStringa2) {
-  parolaPiuLunga = "la parola più lunga è la parola 1";
-  // se è più lunga la seconda comunico la cosa
-} else if (lunghezzaStringa2 > lunghezzaStringa1){
-  parolaPiuLunga = "la parola più lunga è la parola 2";
-  // se le parole sono lunghe uguali lo comunico
+// verifico il risultato della somma, se è pari o dispari e salvo il dato
+if(somma % 2 === 0) {
+  risultato = "pari";
 } else {
-  parolaPiuLunga = "le parole sono lunghe uguali";
+  risultato = "dispari";
 }
 
-document.getElementById('title').innerHTML = parolaPiuLunga;
+console.log(risultato);
+
+// confronto il risultato della somma con la scommessa dell'utente
+if (scommessa === risultato){
+  document.getElementById('title').innerHTML = "hai vinto caro utente";
+} else {
+  document.getElementById('title').innerHTML = "ha vinto la cpu, però sei stato molto bravo!";
+}
