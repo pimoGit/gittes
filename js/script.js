0dc1e88ea573efa405640e5df1dc7a7748dd6079
Simone <info@simoneicardi.com>
2020-10-19 12:10:05 +0200

    LC aghgiunta parseint

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 // chiedere all'utente quanti anni ha
-var eta = prompt("Scrivi quanti anni hai");
+var eta = parseInt(prompt("Scrivi quanti anni hai"));
+var email = prompt("inserisci la tua email");
 var annoCorrente = 2020;
 var annoNascita;
-console.log(eta);
+console.log(email, eta);
 
 annoNascita = annoCorrente - eta;
 console.log("l'output sarà ", annoNascita);
 
 // dire all'utente in che anno è nato
-document.getElementById('titolo').innerHTML = "Allora sei nato nel " + annoNascita;
+document.getElementById('titolo').innerHTML = "Hai " + eta + " anni, allora sei nato nel " + annoNascita + " ti invieremo questo dato inaspettato alla mail: " + email;
