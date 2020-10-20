12d64e8c82700e146db71cd20f30dac03741ac2c
Simone <info@simoneicardi.com>
2020-10-20 12:47:45 +0200

    LC età più alta ex js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-// Quanti minuti mancano alla fine dell’ora?
-var dataUtente = new Date();
+// Inserisci con due prompt l’età di due persone (persona1, persona2)
+// e stampa a schermo il numero più grande.
 
-console.log(dataUtente);
+var etaUno = parseInt(prompt("inserisci la tua età"));
+var etaDue = parseInt(prompt("inserisci la tua età"));
 
-var minutiRimanenti = 60 - dataUtente.getMinutes();
-
-console.log(minutiRimanenti);
+if (etaUno > etaDue) {
+  console.log(etaUno);
+} else if (etaUno < etaDue) {
+  console.log(etaDue);
+} else {
+  console.log("le età sono uguali");
+}
