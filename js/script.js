fde23b25c1f21d541c9cb581c96db07824838ed3
Simone <info@simoneicardi.com>
2020-10-20 10:54:38 +0200

    LC ex parola più lunga con uso condizioni js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //  DESCRIZIONE EX: Inserisci due parole, quale è la piu lunga?
 
+var msg;
 
 // input: utente inserisce  parola uno
 var parola1 = prompt("inserisci la prima parola");
@@ -13,13 +14,16 @@ console.log(parola1.length, parola2.length);
   // se parola1 è più lunga
   if (parola1.length > parola2.length){
     // creo messaggio corrispondente
+    msg = "parola1 è più lunga";
     console.log("parola1 è più lunga");
   } else if (parola2.length > parola1.length) { // se parola2 è più lunga
    //creo messaggio corrispondente
-   console.log("parola2 è più lunga");
+    msg = "parola2 è più lunga";
+    console.log("parola2 è più lunga");
   } else { // altrimenti se sono ugulai
      // creo messaggio corrispondente
-     console.log("le parole sono lunghe uguali");
+     msg = "le parole sono lunghe uguali";
+    console.log("le parole sono lunghe uguali");
   }
 
 
@@ -27,3 +31,4 @@ console.log(parola1.length, parola2.length);
 
 
 // output: per dire quale parola è più lunga
+document.getElementById('titolo').innerHTML = msg;
