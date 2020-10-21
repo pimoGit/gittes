edc2ff779bc2d3c47c106e9fdf2d94df5207ced3
Simone <info@simoneicardi.com>
2020-10-21 12:18:14 +0200

    LC uso claasi dinamiche

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-var element = document.getElementById("titolo");
+// Il software deve chiedere all’utente il suo nome e il sesso
+// con due prompt diversi.
+// Sulla pagina html deve apparire “Ciao <nome>”,
+// il colore del nome deve essere azzurro o rosa a seconda del sesso inserito
 
+// selezione elemento
+var el = document.getElementById('titolo');
 
+// chiedo nome nomeutente
+var nome = prompt("inserisci il nome");
 
-element.addEventListener('click',
- function() {
-   // codice
-   var numero = Math.random() * 10;
-   alert("il numero uscito è: " + numero);
-} );
+// chiedo sesso utente
+var sesso = prompt("inserisci il sesso M o F");
+
+// output nome in pagina
+el.innerHTML = "Ciao " + nome;
+
+// gestire colore output
+if (sesso === "M") {
+  el.className = "blu";
+} else {
+  el.className = "rosa";
+}
