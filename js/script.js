86d9890eb83702b2f4f19abd184df1bc35f0bdb1
Simone <info@simoneicardi.com>
2020-10-21 11:47:56 +0200

    LC dom manipulation step1

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 // Inserisci con due prompt l’età di due persone (persona1, persona2)
 // e stampa a schermo il numero più grande.
 
-var etaUno = parseInt(prompt("inserisci la tua età"));
-var etaDue = parseInt(prompt("inserisci la tua età"));
 
-if (etaUno > etaDue) {
-  console.log(etaUno);
-} else if (etaUno < etaDue) {
-  console.log(etaDue);
-} else {
-  console.log("le età sono uguali");
-}
+
+var elemento = document.getElementById('nomeutente');
+
+var nome = prompt("inserisci il tuo nome");
+
+elemento.innerHTML = nome;
+
+
+elemento.style.color = "green";
+
+elemento.className = elemento.classList + " redText";
