5fc4c96c9f82e49eac66da88d5d15ab5535e3c6a
Simone <info@simoneicardi.com>
2020-10-22 11:05:19 +0200

    LC uso array step1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
-// Chiedi un numero all’utente e stampa in console il numero successivo.
+// Crea un array contenente 3 nomi.
+// Chiedi un numero da 0 a 2 all’utente e stampa l’elemento dell’array corrispondente.
 
-var numero = parseInt(prompt("numero grazie"));
+var mioArray = ["nome0", "nome1", "nome2"];
 
+var indice = parseInt(prompt("dammi un indice tra 0 e 2"));
 
-// numero = numero + 1; // versione estesa
-// numero++; // versione compatta
+var arrayItem = mioArray[indice];
 
-console.log(++numero);
-console.log(numero);
+console.log("array completo: ",mioArray);
+console.log("item selezionato: ", arrayItem);
+
+console.log("lunghezza iniziale array: ", mioArray.length);
+
+mioArray.push("nome3");
+
+console.log("array completo dopo 1 push: ", mioArray);
+
+console.log("array completo: ", mioArray.length);
