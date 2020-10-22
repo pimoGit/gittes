6fb0e6720be1298779bc7ab531c2613a6eccea99
Simone <info@simoneicardi.com>
2020-10-22 12:18:53 +0200

    LC cicli + array step2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-// Crea un array contenente 5 nomi e stampali a schermo
+// Crea un array di numeri, e stampa solo i numeri dispari
 
-var mioArray = ["nome0", "nome1", "nome2", "nome3", "nome4", 1, 2, 3];
+var numeri = [1,3,2,4,5,6,7,8];
 
 
-// console.log(mioArray[0]);
-// console.log(mioArray[1]);
-// console.log(mioArray[2]);
-// console.log(mioArray[3]);
-// console.log(mioArray[4]);
-
-for (var i = 0; i < mioArray.length; i++){
-  var arrayItemIesimo = mioArray[i];
-  console.log(arrayItemIesimo);
+for (var i = 0; i < numeri.length; i++){
+  var arrayItem = numeri[i];
+  if (arrayItem % 2 !== 0){
+    console.log(arrayItem);
+  }
 }
