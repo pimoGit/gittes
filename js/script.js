164fa703cfa91db68e97ce1013f9df7e2a2226ad
Simone <info@simoneicardi.com>
2020-10-23 11:38:43 +0200

    LC esempio while e do while

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-// Crea un array di numeri, e stampa solo i numeri dispari
 
-var numeri = [1,3,2,4,5,6,7,8];
+var nomi = ["nome1","nome2","nome3","nome4","nome5"];
 
-
-for (var i = 0; i < numeri.length; i++){
-  var arrayItem = numeri[i];
-  if (arrayItem % 2 !== 0){
-    console.log(arrayItem);
-  }
+/*
+for (var i = 0; i < nomi.length; i++){
+  console.log(nomi[i]);
 }
+*/
+
+var i = 10;
+while (i < nomi.length){
+  console.log(nomi[i]);
+
+   i++;
+}
+
+
+var i = 10;
+do {
+  console.log(nomi[i]);
+
+   i++;
+} while (i < nomi.length);
