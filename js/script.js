a6d0d0614d7968235cdc8613d86f788308bf6831
Simone <info@simoneicardi.com>
2020-10-28 12:25:24 +0100

    LC var scope 2

--- js/script.js
+++ js/script.js
@@ -1,26 +1,21 @@
-// a = 0;
-// b = "buongiorno";
-// var c = "ciao";
+'use strict';
 
-var varGlobale = "mi vedono tutti";
 
-console.log(saluta());
-// console.log("ilvalore ritornato dalla funzione è: ", valoreRitornato);
-//
-// console.log("a is " + a);
-// console.log("b is " + b);
+a = 0;
+b = "buongiorno";
+var c = "ciao";
+
+saluta();
+
+console.log("a is " + a);
+console.log("b is " + b);
 
 function saluta() {
-  // var d = "asd";
-  // console.log("a is " + a);
-  // var b = a + 1;
-  // console.log("b is " + b);
-  // console.log(c);
-  var varInterna = "non puoi vedermi da fuori!!";
-  console.log("la var interna cercata dallo scope locale è: ", varInterna);
-  console.log("la var globale cercata dallo scope locale è: ", varGlobale);
-
-  return varInterna;
+  varGlob = 5;
+  var d = "asd";
+  console.log("a is " + a);
+  var b = a + 1;
+  b = b + 11;
+  console.log("b is " + b);
+  console.log(c);
 }
-
-console.log("la var interna cercata dallo scope globale è: ", varInterna);
