6bee2b31b0370866176bd1ebbdb93a80f5711c46
Simone <info@simoneicardi.com>
2020-11-02 12:08:16 +0100

    LC eventi jQ e funzioni anonime VS nominali

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 // Creare un bottone che on hover aggiunga una classe che cambia il colore del testo e la toglie quando il mouse esce dal bottone.
-
+ var noemVar = false;
 
 var bottone = $('button');
 
 console.log(bottone);
 
+
+
 bottone.mouseenter(function() {
   bottone.addClass("red");
+  noemVar = true;
   }
 );
+
 
 bottone.mouseleave(function() {
   bottone.removeClass("red");
@@ -16,9 +20,25 @@ bottone.mouseleave(function() {
 
 // Con il doppio click cambio il testo in “cliccato”.
 // Con un click solo non fa niente
+// versione function anonima
+/*
 bottone.dblclick(function() {
- // console.log(bottone.text());
+ console.log(bottone.text());
  bottone.text("cliccato000oooooooooo!");
-
+ alert("hofatto tutto!");
   }
 );
+*/
+
+// versione function nominale
+bottone.dblclick(cbDclick);
+
+
+
+// DEFINIZIONE FUNZIONI
+
+function cbDclick() {
+ console.log(bottone.text());
+ bottone.text("cliccato000oooooooooo!");
+ alert("hofatto tutto!");
+}
