1e27efa4a611bff64223a443d4566215dc9e9016
Simone <info@simoneicardi.com>
2020-11-02 12:22:02 +0100

    LC gestione eventi jQ con var state

--- js/script.js
+++ js/script.js
@@ -1,44 +1,14 @@
-// Creare un bottone che on hover aggiunga una classe che cambia il colore del testo e la toglie quando il mouse esce dal bottone.
- var noemVar = false;
+ // Creare il titolo di un paragrafo e un bottone che permette di espandere il testo inizialmente nascosto
 
-var bottone = $('button');
+$('p').hide();
+var aperto = false;
 
-console.log(bottone);
-
-
-
-bottone.mouseenter(function() {
-  bottone.addClass("red");
-  noemVar = true;
+$('button').click(function() {
+  if(aperto === false){
+    $('p').show();
+    aperto = true;
+  } else {
+    $('p').hide();
+    aperto = false;
   }
-);
-
-
-bottone.mouseleave(function() {
-  bottone.removeClass("red");
 });
-
-// Con il doppio click cambio il testo in “cliccato”.
-// Con un click solo non fa niente
-// versione function anonima
-/*
-bottone.dblclick(function() {
- console.log(bottone.text());
- bottone.text("cliccato000oooooooooo!");
- alert("hofatto tutto!");
-  }
-);
-*/
-
-// versione function nominale
-bottone.dblclick(cbDclick);
-
-
-
-// DEFINIZIONE FUNZIONI
-
-function cbDclick() {
- console.log(bottone.text());
- bottone.text("cliccato000oooooooooo!");
- alert("hofatto tutto!");
-}
