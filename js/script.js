bdaf4ca11bef461b148c28d728fc34d62eae66ad
Simone <info@simoneicardi.com>
2020-11-03 10:26:44 +0100

    LC uso this in jQ

--- js/script.js
+++ js/script.js
@@ -2,28 +2,10 @@
 $(document).ready(function () {
 
 
-  //$('p').hide();
-
-/*
-  var aperto = false;
-
-  $('button').click(function() {
-   if(aperto === false){
-     $('p').show();
-     aperto = true;
-   } else {
-     $('p').hide();
-     aperto = false;
-   }
- });
-*/
-
- $('button').click(function() {
-    $('p').toggle(1000, function () {
-      alert('Testo gestito');
-});
-    //$('button').toggleClass("classe");
- });
+  $('.buttons').click(function () {
+    $(this).toggleClass("qualsiasi");
+    //$('.buttons').hide();
+  });
 
 
 
