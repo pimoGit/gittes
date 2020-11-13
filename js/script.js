13f58a0db63db6cc8e16e6da6302846f4e6febb1
Simone <info@simoneicardi.com>
2020-11-13 12:30:15 +0100

    LC gattini step1

--- js/script.js
+++ js/script.js
@@ -1,37 +1,13 @@
-const students = [3, 4, 6, 7, 9, 11];
-
-
-// versione con il for
-/*
-var newArr = [];
-
-for (let i = 0; i < students.length; i++) {
-  let element = students[i];
-  console.log(element);
-  if(element === "Paolo"){
-    newArr.push(element);
-  }
-}
-
-console.log(students, newArr);
-*/
-
-
-
-// equivalente con filter
-const newArr = students.filter((element) => element % 2 !== 0);
-
-console.log(students, newArr);
-
-
 /*
 icone:
 <i class="fas fa-cat" style="color:......"></i>
  <i class="fas fa-ribbon" style="color:.....; filter: opacity(....);"> </i>
  nome gatto
+*/
 
 //abbiamo dei gattini
-const cats = [{
+const cats = [
+  {
     name: 'Adam',
     age: 0.3,
     color: '#00ffff',
@@ -61,18 +37,23 @@ const cats = [{
 const container = document.getElementById('container');
 
 // stampiamo tutti i gattini in fila nel 'container' [forEach + Tlit]
-cats.forEach((item, i) => {
+cats.forEach((element) => {
   let contenuto = container.innerHTML;
-  console.log(contenuto);
+
+  //console.log(contenuto);
+
   container.innerHTML = ` ${contenuto}
-  <div>
-    <i class="fas fa-cat" style="color:${item.color}"></i>
-    ${item.name}
-  </div>  `
+    <div>
+      <i class="fas fa-cat" style="color:${element.color}"></i>
+      ${element.name}
+    </div>
+  `
 });
 
 
-//mettiamo un fiocco colorato ad ogni gattino base rosa femmine
+
+//mettiamo un fiocco colorato ad ogni gattino,
+//base rosa femmine
 //base blue maschi,
 //il fiocco deve essere più chiaro se più giovane più scuro se più vecchio
 //creaiamo solo dati ma output facciam dopo divisi
@@ -81,10 +62,28 @@ const pink = '#ff00e6';
 const blue = '#0084ff';
 //usiamo map per creare nuovo array con aggiunta prop ribbon {color, opacity} [..rest/destructuring opTernario]
 // opacity sarà in base all'age [age*100]
+var ribbonCats = cats.map((element) =>{
+  return {
+    ...element,
+    ribbon: {
+      color: (element.gender === "female" ? pink  :  blue), // if (element.gender === "female") { pink } else { blue }
+      opacity: (element.age * 100)
+    }
+  }
+});
+
+console.log(cats, ribbonCats);
+
+
+
+
 
 
 // dividiamoli per sesso [2 nuovi array con filter]
-
+const femaleCats = ribbonCats.filter((element) => element.gender === "female" );
+console.log(femaleCats);
+const maleCats = ribbonCats.filter((element) => element.gender === "male" );
+console.log(maleCats);
 
 
 // e facciamo output femmine in 'female'
@@ -92,18 +91,31 @@ const blue = '#0084ff';
 const femaleContainer = document.getElementById('female');
 const maleContainer = document.getElementById('male');
 
+femaleCats.forEach((element) => {
+  let contenuto = femaleContainer.innerHTML;
+
+  femaleContainer.innerHTML = ` ${contenuto}
+    <div>
+      <i class="fas fa-cat" style="color:${element.color}"></i>
+      <i class="fas fa-ribbon" style="color:${element.ribbon.color}; filter: opacity(${element.ribbon.opacity}%);"> </i>
+      ${element.name}
+    </div>
+  `
+});
+
 
 
 
 //creiamo una versione [nuovo arr] con prima femmine poi maschi [spread-rest]
 
 
+
+
 // ne facciamo output in 'container-new'
 const containerNew = document.getElementById('container-new');
-
 // stampiamo tutti i gattini in fila
 
 
-//altro array con obj di soli ribbon and name [destructuring in map]
 
-*/
+
+//altro array con obj di soli ribbon and name [destructuring in map]
