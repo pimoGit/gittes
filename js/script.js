24f2795948d30bbfe091a22ae9aada6c58a61eef
Simone <info@simoneicardi.com>
2020-11-17 12:34:36 +0100

    LC slider next button con Vue

--- js/script.js
+++ js/script.js
@@ -3,16 +3,22 @@
 var app = new Vue({
   el: "#app",
   data: {
-    titleShow: "A",
-    names: ['Michele', 'Fabio', 'Roberto']
+    fotoIndex: 0,
+    foto: [
+      "img/image1.jpg",
+      "img/image2.jpg",
+      "img/image3.jpg",
+      "img/image4.jpg"
+    ]
   },
-  methods: {
-    toggleVisible: function() {
-      if(this.titleShow === "A"){
-        this.titleShow = "B";
-      } else {
-        this.titleShow = "A";
-      }
-    }
+   methods: {
+     nextFoto: function () {
+       this.fotoIndex = this.fotoIndex + 1;
+
+       if(this.fotoIndex > (this.foto.length - 1)){
+         this.fotoIndex = 0;
+       }
+       console.log(this.fotoIndex);
+     }
   }
 })
