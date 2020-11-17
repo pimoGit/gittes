0d89d9e93f411583bfecede1b8df9a0d27255f6d
Simone <info@simoneicardi.com>
2020-11-17 11:06:07 +0100

    LC uso V-if Vue

--- js/script.js
+++ js/script.js
@@ -1,24 +1,17 @@
-// Utilizzare due input, in cui inseriremo Nome e Cognome.
-// In un paragrafo utilizzare Nome e Cognome per salutare l’utente
-
-// istanziamo Vue agganciando l'istanza al nostro contenitore
+// slider con Vue
 
 var app = new Vue({
   el: "#app",
   data: {
-    nome: "nome",
-    cognome: "cognome",
-    mieClassi: "classe2"
+    titleShow: false
   },
   methods: {
-    changeTitle: changeTitle() {
-      //alert("ciao dal metodo interno");
-      if (this.mieClassi === "classe2"){
-        this.mieClassi = "classe1";
+    toggleVisible: function() {
+      if(this.titleShow === true){
+        this.titleShow = false;
       } else {
-        this.mieClassi = "classe2";
+        this.titleShow = true;
       }
-      console.log(this.cognome);
     }
   }
-});
+})
