9abd48a33438041ea3702400ea58705cd3f4d586
Simone <info@simoneicardi.com>
2020-11-24 12:38:03 +0100

    BoolZapp milestone2 con Vue, struttura (mancano img e css da un errore)

--- js/script.js
+++ js/script.js
@@ -1,24 +1,102 @@
-// slider con Vue
-
 var app = new Vue({
-  el: "#app",
-  data: {
-    fotoIndex: 0,
-    foto: [
-      "img/image1.jpg",
-      "img/image2.jpg",
-      "img/image3.jpg",
-      "img/image4.jpg"
-    ]
-  },
-   methods: {
-     nextFoto: function () {
-       this.fotoIndex = this.fotoIndex + 1;
-
-       if(this.fotoIndex > (this.foto.length - 1)){
-         this.fotoIndex = 0;
-       }
-       console.log(this.fotoIndex);
-     }
-  }
-})
+        el: '#app',
+        data: {
+            user: {
+                name: 'Nome Utente',
+                avatar: '_io'
+            },
+            contacts: [
+                {
+                    name: 'Michele',
+                    avatar: '_1',
+                    visible: true,
+                    messages: [{
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di dargli da mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Fabio',
+                    avatar: '_2',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            message: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            message: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Samuele',
+                    avatar: '_3',
+                    visible: true,
+                    messages: [{
+                        date: '28/03/2020 10:10:40',
+                        message: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            message: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            message: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Luisa',
+                    avatar: '_4',
+                    visible: true,
+                    messages: [{
+                        date: '10/01/2020 15:30:55',
+                        message: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+            ],
+            contactActive: 0,
+        },
+        mounted() {
+        },
+        methods: {
+            
+            setActiveConversation(index) {
+                //rendiamo visibili i messaggi di un contatto
+                this.contactActive = index;
+            },
+        }
+    },
+)
