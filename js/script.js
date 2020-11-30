7d332f6c797629e2064fbbdbad2979f1c7b341e8
Simone <info@simoneicardi.com>
2020-11-30 12:57:56 +0100

    LC uso di axios con Vue step 2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-
+ // Gioca contro il PC a chi ha il numero più grande
 
 var app = new Vue({
   el: "#app",
   data: {
-    number: 0
+    numberUser: 0,
+    numberPc: 0
   },
   mounted: function () {
     //let self = this;
     axios.get('https://flynn.boolean.careers/exercises/api/random/int')
     .then(risposta => {
-       console.log(risposta.data.response);
-       this.number = risposta.data.response;
+       this.numberUser = risposta.data.response;
      });
+
+     axios.get('https://flynn.boolean.careers/exercises/api/random/int')
+     .then(risposta => {
+        this.numberPc = risposta.data.response;
+      });
    }
 });
