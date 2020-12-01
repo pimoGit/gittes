dcbdae4536ceacfecb073b6627e09c183f17661f
Simone <info@simoneicardi.com>
2020-12-01 11:52:02 +0100

    LC ex quadratini step1

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
- // Gioca contro il PC a chi ha il numero più grande
+// Griglia 6x6, ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9.
+// Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde. Il numero ottenuto appare al centro del quadrato
+const randNUm = "https://flynn.boolean.careers/exercises/api/random/int";
 
 var app = new Vue({
-  el: "#app",
+  el: '#app',
   data: {
-    numberUser: 0,
-    numberPc: 0
+    numbers: []
   },
   mounted: function () {
-    //let self = this;
-    axios.get('https://flynn.boolean.careers/exercises/api/random/int')
-    .then(risposta => {
-       this.numberUser = risposta.data.response;
-     });
+    for (let i = 0; i < 36; i++) {
+      this.numbers.push('');
+    }
+    //this.faiqualcosa();
+  },
+  methods: {
+    generaNumero: function () {
+      axios.get(randNUm)
+      .then(risposta => {
+        let pushedNum = risposta.data.response;
+        //this.numbers[0] = pushedNum;
+        this.$set(this.numbers, 0, pushedNum);
+        // console.log(pushedNum);
+        console.log(this.numbers);
+      })
+    },
+    faiqualcosa: function () {
+      alert("ho fatto qualcosa!");
+    }
 
-     axios.get('https://flynn.boolean.careers/exercises/api/random/int')
-     .then(risposta => {
-        this.numberPc = risposta.data.response;
-      });
-   }
+  }
+
 });
